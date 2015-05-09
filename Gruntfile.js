72bcc441581faec9e4d77b104adf145e77475b5c
Paulo McNally <paulomcnally@gmail.com>
2015-05-08 23:18:20 -0600

    added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    'loopback_auto': {
+      'db_autoupdate': {
+        options: {
+          dataSource: 'db',
+          app: './server/server',
+          config: './server/model-config',
+          method: 'autoupdate'
+        }
+      }
+  });
+  // Load the plugin 
+  grunt.loadNpmTasks('grunt-loopback-auto');
+  grunt.registerTask('default', ['loopback_auto']);
+};
